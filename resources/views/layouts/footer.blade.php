baaa9d15f9fdbbc9e154518b338d03317d3f2aec
Keziah Mae S. Tingga-an <keziahmaeee@gmail.com>
2026-06-17 09:37:44 +0800

    add: footer view

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="d-flex justify-content-end align-items-center">
+        <div class="text-end me-2 lh-1">
+            <small> In Collaboration With </small> <br> <strong>Saint Louis University</strong>
+        </div>
+        <img src="{{ asset('images/slu-logo.png') }}" width="35">
+    </div>
+</footer>
