dbe799b5560458df069b3e3b4ac27b2edcf2717d
Keziah Mae S. Tingga-an <keziahmaeee@gmail.com>
2026-06-17 13:48:00 +0800

    update: add browser icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Montserrat:wght@500;600;700;800&display=swap"
           rel="stylesheet">
+    <link rel="icon" type="image/png" href="{{ asset('images/DENR-logo.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
         rel="stylesheet">
     <link rel="stylesheet"
